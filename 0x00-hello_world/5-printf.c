27fcc02ee771d6c3f07b8ce206b2120be85ba79e
kay <okodikec@gmail.com>
2022-04-07 04:45:41 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art");
+return (0);
+}
